00ba817008e1cb7cde03b32e20e968d72305cb10
Kurt Nelson <kurtn@uber.com>
2020-12-22 02:44:20 -0800

    Hack the query for now

--- src/components/closest.jsx
+++ src/components/closest.jsx
@@ -11,6 +11,8 @@ class Closest extends React.Component {
       isLoaded: false,
       parks: [],
     }
+    this.db = firebase.firestore()
+    this.db.enablePersistence()
   }
 
   static get propTypes() {
@@ -31,7 +33,7 @@ class Closest extends React.Component {
   }
 
   fetchParks() {
-    fetchNearby(this.props.lat, this.props.lng)
+    this.fetchNearby(this.props.lat, this.props.lng)
       .then(parks => {
         this.setState({ parks: parks, isLoaded: true })
       })
@@ -56,7 +58,7 @@ class Closest extends React.Component {
           <p>Your closest parks are:</p>
           <ul>
             {parks.map(park => (
-              <li key={park.globalid}>{park.map_label}</li>
+              <li key={park.id}>{park.data.map_label}</li>
             ))}
           </ul>
           <p>
@@ -66,53 +68,57 @@ class Closest extends React.Component {
       )
     }
   }
+
+  fetchNearby(lat, lng) {
+    const center = [lat, lng]
+    const radiusInKm = 0.5
+
+    const bounds = geohashQueryBounds(center, radiusInKm)
+    const promises = []
+
+    // TODO: Figure out why the Firestore example for geohash search is broken.
+    //for (const b of bounds) {
+    const q = this.db.collection("parks").orderBy("geohash")
+    //.startAt(b[0])
+    //.endAt(b[1])
+
+    //console.log("query: " + b[0] + " " + b[1])
+    promises.push(q.get())
+    //}
+
+    // Collect all the query results together into a single list
+    return Promise.all(promises)
+      .then(snapshots => {
+        const matchingDocs = new Map()
+        console.log("snapshots: " + snapshots.length)
+
+        for (const snap of snapshots) {
+          console.log("docs: " + snap.docs.length)
+          for (const doc of snap.docs) {
+            const location = doc.get("location")
+
+            // We have to filter out a few false positives due to GeoHash
+            // accuracy, but most will match
+            const distance = distanceBetween(
+              [location.latitude, location.longitude],
+              center
+            )
+            if (distance <= radiusInKm) {
+              matchingDocs.set(doc.id, doc.data())
+            }
+          }
+        }
+        console.log("close: " + matchingDocs.size)
+        return matchingDocs
+      })
+      .then(docs => {
+        const parks = []
+        for (let [id, park] of docs.entries()) {
+          parks.push({ id: id, data: park })
+        }
+        return parks
+      })
+  }
 }
 
 export default Closest
-
-function fetchNearby(lat, lng) {
-  const db = firebase.firestore()
-  db.enablePersistence()
-  const center = [lat, lng]
-  const radiusInKm = 0.5
-
-  const bounds = geohashQueryBounds(center, radiusInKm)
-  const promises = []
-  for (const b of bounds) {
-    const q = db
-      .collection("parks")
-      .orderBy("geohash")
-      .startAt(b[0])
-      .endAt(b[1])
-
-    console.log("query: " + b[0] + " " + b[1])
-    promises.push(q.get())
-  }
-
-  // Collect all the query results together into a single list
-  return Promise.all(promises).then(snapshots => {
-    const matchingDocs = []
-    console.log("snapshots: " + snapshots.length)
-
-    for (const snap of snapshots) {
-      console.log("docs: " + snap.docs.length)
-      for (const doc of snap.docs) {
-        const location = doc.get("location")
-
-        // We have to filter out a few false positives due to GeoHash
-        // accuracy, but most will match
-        const distance = distanceBetween(
-          [location.latitude, location.longitude],
-          center
-        )
-        if (distance <= radiusInKm) {
-          matchingDocs.push(doc)
-        } else {
-          console.log("Throwing out " + doc.get("map_label"))
-        }
-      }
-    }
-
-    return matchingDocs
-  })
-}
