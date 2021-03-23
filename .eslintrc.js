137ef29ffafa641aa0448c95331c449b9b4eea01
Kurt Nelson <kurtisnelson@gmail.com>
2021-03-23 12:11:33 -0700

    Better eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,21 @@
 module.exports = {
   env: {
-    commonjs: true,
-    es6: true,
+    browser: true,
+    es2021: true,
     node: true,
   },
   extends: ["plugin:react/recommended", "google", "prettier"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
+    ecmaVersion: 12,
     sourceType: "module",
-    ecmaVersion: 2018,
   },
   plugins: ["react"],
   rules: {
     "require-jsdoc": 0,
   },
-  overrides: [
-    {
-      files: ["src/*.jsx", "src/*.js"],
-    },
-  ],
   settings: {
     react: {
       version: "detect",
